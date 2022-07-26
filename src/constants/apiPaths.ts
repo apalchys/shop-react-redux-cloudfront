2022640764892b2a5b668fcdf903940facecd07a
Viktar-Kavaliou <viktar_kavaliou@epam.com>
2022-07-26 17:44:02 +0400

    add import service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://2uqvkwebjd.execute-api.eu-west-3.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://cr4hljpz4f.execute-api.eu-west-3.amazonaws.com/dev',
   bff: 'https://2uqvkwebjd.execute-api.eu-west-3.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
